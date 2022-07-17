376efb35af7e6e3614383ae72b447873820e0819
Adam <34453316+doogiski@users.noreply.github.com>
2022-07-16 19:14:28 -0700

    Create nhl_player_toi.py

---
+++ scripts/nhl_player_toi.py
@@ -0,0 +1,26 @@
+import requests
+import math
+from flatten_json import flatten
+import pandas as pd
+
+year = '20212022'
+
+url = f'https://api.nhle.com/stats/rest/en/skater/timeonice?isAggregate=false&isGame=false&sort=%5B%7B%22property%22:%22otTimeOnIcePerOtGame%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22playerId%22,%22direction%22:%22ASC%22%7D%5D&start=0&limit=100&factCayenneExp=gamesPlayed%3E=1&cayenneExp=gameTypeId=2%20and%20seasonId%3C={year}%20and%20seasonId%3E={year}'
+
+r = requests.get(url)
+count = r.json()['total']
+
+#Count number of pages based on 100 record limit per page
+pages = math.ceil(count/100)
+
+dic_flattened = []
+for i in range(pages):
+    print(i*100)
+    url_paged = f'https://api.nhle.com/stats/rest/en/skater/timeonice?isAggregate=false&isGame=false&sort=%5B%7B%22property%22:%22otTimeOnIcePerOtGame%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22playerId%22,%22direction%22:%22ASC%22%7D%5D&start={i*100}&limit=100&factCayenneExp=gamesPlayed%3E=1&cayenneExp=gameTypeId=2%20and%20seasonId%3C={year}%20and%20seasonId%3E={year}'
+    r = requests.get(url_paged)
+    data = r.json()['data']
+    for d in data:
+        dic_flattened.append(flatten(d))
+        
+df = pd.DataFrame(dic_flattened)
+df.to_csv('2021-2022_NHLcom_toi.csv',index=False,encoding='utf-8-sig')
